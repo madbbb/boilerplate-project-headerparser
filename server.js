4ae847861eeeb8fccc04809b622645dab9c3e52c
madbbb <madbbb@icloud.com>
2019-03-23 12:01:07 +0200

    Initial

--- server.js
+++ server.js
@@ -18,15 +18,21 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    "ipaddress": req.header('x-forwarded-for'),
+    "language": req.header('accept-language'),
+    "software": req.header('user-agent')
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
